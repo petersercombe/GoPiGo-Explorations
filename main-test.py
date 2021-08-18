9b6279ee3cff1bd5911f1e54deedc4a2c512239c
petersercombe <peter.sercombe@bcc.net.au>
2021-08-19 08:49:21 +1000

    SLow main loop down.

--- main-test.py
+++ main-test.py
@@ -1,10 +1,12 @@
 from motor import *
 import keyboardController as kc
+from time import sleep
 
 kc.init()
 
 while True:
     throttle, steering = kc.main()
     motor(throttle, steering)
+    sleep(0.05)
     # Capture image from camera
     # Save throttle/steering values with image
