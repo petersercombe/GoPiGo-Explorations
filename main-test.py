047e3499edc122950025faf2241e3a677da7ab2b
petersercombe <peter.sercombe@bcc.net.au>
2021-09-02 09:33:30 +1000

    Tidying up

--- main-test.py
+++ main-test.py
@@ -6,6 +6,8 @@ kc.init()
 
 while True:
     throttle, steering = kc.main()
+    if throttle > 0 and dist.read() < 5:
+        throttle = 0
     motor(throttle, steering)
     sleep(0.05)
     # Capture image from camera
