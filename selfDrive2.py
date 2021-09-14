91ed280916c8ced423ddd3c7e7476ef5048d74e3
petersercombe <peter.sercombe@bcc.net.au>
2021-09-15 08:50:10 +1000

    selfDrive2.py updated to process images for use with black and white model.

--- selfDrive2.py
+++ selfDrive2.py
@@ -1,5 +1,6 @@
 # Import Pi Camera library
 from picamera import PiCamera
+from picamera.array import PiRGBArray
 
 from time import sleep
 import datetime as dt
@@ -16,7 +17,11 @@ from motor import *
 camera = PiCamera()
 
 # Set size of images - this ideally is the same as the model used to train the images.
-imageSize = (160, 120)
+imageSize = (160, 128)
+
+rawCapture = PiRGBArray(camera, size=imageSize)
+sleep(2)
+
 
 # Load Lobe TF Lite model
 # --> Change model path
@@ -31,18 +36,22 @@ if __name__ == '__main__':
 
     # Adjust speed factor if your bot is over or under steering.
     speedFactor = 0.8
-    
-    while True:
-        stream = io.BytesIO() # Capture image to an in-memory stream
-        camera.capture(stream, format='jpeg', resize=imageSize, use_video_port=True)
+
+    for frame in camera.capture_continuous(rawCapture, format='bgr', use_video_port=True, resize=imageSize):
+    #while True:
+        #stream = io.BytesIO() # Capture image to an in-memory stream
+        #camera.capture(stream, format='jpeg', resize=imageSize, use_video_port=True)
         # "Rewind" the stream to the beginning so we can read its content
-        stream.seek(0)
+        #stream.seek(0)
+        image = frame.array
 
         start = dt.datetime.now()  # Capture start time
+        gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+        #image = Image.open(stream).convert('L') # .convert('L') opens the image as greyscale
 
-        image = Image.open(stream).convert('L') # .convert('L') opens the image as greyscale
+
         # Apply slight blur to image to soften edges
-        blurred = cv2.GaussianBlur(image, (3, 3), 0)
+        blurred = cv2.GaussianBlur(gray, (3, 3), 0)
         # Convert to binary black and white using adaptive threshold method
         bnw = cv2.adaptiveThreshold(blurred, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 21, 13)
 
@@ -61,7 +70,8 @@ if __name__ == '__main__':
         motor(speedFactor, steering * speedFactor)
 
         # Clear the stream in preparation for the next frame
-        stream.truncate()
+        #stream.truncate()
+        rawCapture.truncate(0)
 
         key = cv2.waitKey(1) & 0xFF
 
